cdaf2815b8d4c02e135dc718deca767c64e569dc
Lucas-Amorim-Almeida <si.lucas.almeida@gmail.com>
2024-10-02 11:02:58 -0300

    feat: add classes that make database manipulation to users and product

---
+++ src/database/ProductConnection.ts
@@ -0,0 +1,47 @@
+import { ProductType } from "@/@types/types";
+import client from "./client";
+import Product from "@/core/Product";
+
+export default class ProductConnection {
+  public static async createProduct(product: Product) {
+    const newProduct = await client.product.create({
+      data: product.getProduct() as Omit<ProductType, "id">,
+    });
+    return newProduct;
+  }
+
+  public static async getAllProduct() {
+    const productList = await client.product.findMany();
+    return productList;
+  }
+
+  public static async findProduct(productName: string) {
+    const productList = await client.product.findMany({
+      where: { name: productName },
+    });
+    return productList;
+  }
+
+  public static async updateProductInfo(
+    productId: string,
+    productInfo: {
+      name?: string;
+      description?: string;
+      price?: number;
+    },
+  ) {
+    if (!productInfo || Object.keys(productInfo).length === 0) return;
+    const updatedProduct = await client.product.update({
+      where: { id: productId },
+      data: productInfo,
+    });
+    return updatedProduct;
+  }
+
+  public static async deleteProduct(productId: string) {
+    const deletedProduct = await client.product.delete({
+      where: { id: productId },
+    });
+    return deletedProduct;
+  }
+}
---
+++ src/database/UserConnection.ts
@@ -0,0 +1,30 @@
+import User from "@/core/User";
+import client from "./client";
+import { UserType } from "@/@types/types";
+
+export default class UserConnection {
+  public static async createNewUser(user: User) {
+    const newUser = await client.user.create({
+      data: user.getUser() as Omit<UserType, "id">,
+    });
+    return newUser;
+  }
+
+  public static async getUser(username: string) {
+    const user = await client.user.findUnique({ where: { username } });
+    return user;
+  }
+
+  public static async updatePassword(userId: string, newPassword: string) {
+    const updatedUser = await client.user.update({
+      where: { id: userId },
+      data: { password: newPassword },
+    });
+    return updatedUser;
+  }
+
+  public static async deleteUser(userId: string) {
+    const deletedUser = await client.user.delete({ where: { id: userId } });
+    return deletedUser;
+  }
+}
