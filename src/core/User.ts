306924343854702c38934afdc75cc7e88f9970b4
Lucas-Amorim-Almeida <si.lucas.almeida@gmail.com>
2024-10-02 09:53:57 -0300

    feat: add prisma client instance and fix an id issue into User class

--- src/core/User.ts
+++ src/core/User.ts
@@ -2,19 +2,24 @@ import { UserType } from "@/@types/types";
 
 export default class User {
   private user: UserType;
-  constructor(username: string, password: string) {
+  constructor(username: string, password: string, id?: string) {
     this.user = {
-      id: this.idGenerator(),
+      id: id ? id : "",
       username,
       password,
     };
   }
 
-  private idGenerator() {
-    return Math.ceil(Math.random() * 1000000).toString();
+  public setId(id: string) {
+    if (this.user.id !== "") return;
+    this.user.id = id;
   }
 
-  public getUser() {
-    return this.user;
+  public getUser(): UserType | Omit<UserType, "id"> {
+    const userInfo = {
+      username: this.user.username,
+      password: this.user.password,
+    };
+    return this.user.id === "" ? userInfo : this.user;
   }
 }
