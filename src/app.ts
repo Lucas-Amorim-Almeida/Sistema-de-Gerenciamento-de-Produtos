292d6b14ef31ff648295cd77f9b16a64806d35aa
Lucas-Amorim-Almeida <si.lucas.almeida@gmail.com>
2024-11-06 11:04:58 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,18 @@ import router from "./routes/router";
 import { errorsMiddleWare } from "./middlewares/errors.middleware";
 import * as swaggerUi from "swagger-ui-express";
 import * as swaggerDoc from "../docs/swagger.json";
+import * as cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: process.env.CORS_ALLOW_ORIGIN,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  //credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use("/api/v1", router);
 // Configuração do Swagger
